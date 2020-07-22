2dbc3e9b65f16d70d3e0e9eb9a4d3a6113f2d4d7
programmerprime <tmai.prime@gmail.com>
2020-07-22 17:13:09 +0600

    last edit

--- script.js
+++ script.js
@@ -13,21 +13,28 @@
         const depositAmount = document.getElementById('depositAmount').value;
         const depositNumber = parseFloat(depositAmount);
 
-        const currentDeposit = document.getElementById('currentDeposit').innerText;
-        const currentDepositNumber = parseFloat(currentDeposit);
+        // const currentDeposit = document.getElementById('currentDeposit').innerText;
+        // const currentDepositNumber = parseFloat(currentDeposit);
 
-        const totalDeposit = depositNumber + currentDepositNumber;
-        document.getElementById('currentDeposit').innerText = totalDeposit;
+        // const totalDeposit = depositNumber + currentDepositNumber;
+        // document.getElementById('currentDeposit').innerText = totalDeposit;
+
+        updateSpanText("currentDeposit", depositNumber);
+        updateSpanText("currentBalance", depositNumber);
 
         document.getElementById('depositAmount').value = "";
 
-        const currentBalance = document.getElementById("currentBalance").innerText;
-        const currentBalanceNumber = parseFloat(currentBalance);
 
-        const newBalance = depositNumber + currentBalanceNumber;
-
-        document.getElementById("currentBalance").innerText = newBalance;
     });
+
+    function updateSpanText(id, depositNumber){
+        const current = document.getElementById(id).innerText;
+        const currentNumber = parseFloat(current);
+
+        const total = depositNumber + currentNumber;
+
+        document.getElementById(id).innerText = total;
+    }
 
     // withdraw button event handler
 
